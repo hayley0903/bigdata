88d429fb7d4ea4900a737f1cb3aa0ca025b46b19
hayley0903 <hyejeonglee0903@gmail.com>
2024-05-31 11:08:28 +0900

    함수 추가

--- 04.position/0530실습정답/script.js
+++ 04.position/0530실습정답/script.js
@@ -17,18 +17,44 @@ popupCloseBtn.addEventListener('click', function() {
   })
 
 
-//sidebar
+// //sidebar
 let sidebar = document.querySelector('.sidebar');
 let sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
 let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
 
 
-//sidebarOpenBtn을 클릭했을때 
-//sidebar에서 is-active 클레스 추가
-sidebarOpenBtn.addEventListener('click',function(){
-  sidebar.classList.add('is-active')
-})
+// // //sidebarOpenBtn을 클릭했을때 
+// // //sidebar에서 is-active 클레스 추가
+// sidebarOpenBtn.addEventListener('click',function(){
+//   sidebar.classList.add('is-active')})
 
-sidebarCloseBtn.addEventListener('click',function(){
-  sidebar.classList.remove('is-active')
-})
+// sidebarCloseBtn.addEventListener('click',function(){
+//  sidebar.classList.remove('is-active')})
+
+
+//함수이용하여 sidebar 호출
+function openSidebar(){
+  //1.sidebar라는 클래스에 is-active 클래스 추가
+  sidebar.classList.add('is-active');
+}
+
+function closeSidebar() {
+  //1.sidebar라는 클래스에 is-active 클래스 제거
+  sidebar.classList.remove('is-active');
+}
+
+sidebarOpenBtn.addEventListener('click', openSidebar);
+sidebarCloseBtn.addEventListener('click', closeSidebar);
+
+
+
+
+//함수 소괄호 안 : 매개변수
+function plus (num1, num2) {
+  console.log(num1,)
+  console.log(num1 + num2)
+  //실행 할 코드
+}
+
+//함수는 호출을 해야 실행됨
+plus (10, 10);
